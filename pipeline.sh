2e3b284432c040469c7224f30450ceab6b6c14c1
citlalimejiaaa <mejiabaqueirocitlalinabey@gmail.com>
2026-05-29 20:45:57 -0600

    Create pipeline.sh

---
+++ pipeline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Pipeline de Bioinformatica para el Gen BRCA1
+echo "Iniciando analisis del Gen BRCA1..."
+
+# Simulacion de control de calidad con FastQC
+echo "Procesando archivos de secuenciacion FASTQ..."
+echo "Analisis completado con exito."
+
+# Exportar resultados finales
+echo "Exportando summary1.txt y summary2.txt..."
